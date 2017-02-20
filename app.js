05ae04a39f49c368dd84370eda5b2181225336f0
Rolando Cruz <pspnando@me.com>
2017-02-20 10:36:58 -0500

    :zap:

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-#Under construction
+App.run(function() {
+	var hello, commitList;
+
+	Font('Quicksand', '700,400,300');
+
+	App.page = new Page({
+	  backgroundColor: Color.random()
+	});
+
+	commitList = ['e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e'];
+
+	hello = new Text({
+	  text: quoteList[Math.floor(Math.random() * 14)],
+	  width: auto,
+	  fontSize: 30,
+	  fontWeight: 700,
+	  color: white,
+	  spacing: 4
+	});
+
+	hello.center();
+
+	hello.color = Color.random();
+
+});
